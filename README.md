aac6a20f9dd327ebf672bad5edf68394aa8a755e
Carooxl <carolin.kaltwasser@hfg-gmuend.de>
2024-04-16 20:29:32 +0200

    Änderungen alle final 16.4

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # rocket_quartet
 
  A website for rocket quartet cards
+
+Icons:
+<a href="https://www.flaticon.com/free-icons/duration" title="duration icons">Duration icons created by LAFS - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/dimensions" title="dimensions icons">Dimensions icons created by Jesus Chavarria - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/weight" title="weight icons">Weight icons created by Good Ware - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/cost" title="cost icons">Cost icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/year-built" title="year built icons">Year built icons created by bsd - Flaticon</a>
+
+https://www.freepik.com/icon/gauge_511533#fromView=search&page=1&position=75&uuid=d3a549fc-a7d3-4cf5-90f9-b5ad2744466b
+
--- card simple/script/main.js
+++ card simple/script/main.js
@@ -1,44 +1,79 @@
 $(document).ready(function () {
-    const wrapper = $('wrapper');
-    rocketData.array.forEach(rocket => {
+    const iconPath = "images/icons/";
+    const wrapper = $('main');
+   
+    rocketData.forEach(rocket => {
+
+        let bgColor;
+        switch (rocket.group_letter) {
+            case 'A':
+                bgColor = 'rgb(11, 0, 40)'; 
+                break;
+            case 'B':
+                bgColor = 'rgb(83, 30, 97)'; 
+                break;
+            case 'C':
+                bgColor = 'rgb(15, 0, 60)'; 
+                break;
+            case 'D':
+                bgColor = 'rgb(60, 25, 80)'; 
+                break;
+            case 'E':
+                bgColor = 'rgb(43, 15, 51)'; 
+                break;
+            case 'F':
+                bgColor = 'rgb(49, 0, 94)'; 
+                break;
+            case 'G':
+                bgColor = 'rgb(38, 0, 94)'; 
+                break;
+            case 'H':
+                bgColor = 'rgb(37, 18, 64)'; 
+                break;   
+        }
+
         const card =
         $(`<div class="card">
         <div class="card-content">
-            <div class="card-number">$(rocket.groupC1</div>
+            <div class="card-number center-content" style="background-color: ${bgColor};"> ${rocket.group_letter}${rocket.number}</div>
             <div class="card-title">
-               <div class="card-name">Voyager 1</div> 
-               <div class="card-group">Probes</div>
+               <div class="card-name">${rocket.name}</div> 
+               <div class="card-group">${rocket.group}</div>
             </div>
-            <div class="rocket-image">
-                <img src="images/voyager.webp" alt="Voyager 1">
-            </div>
-            <div class="goal-icon">Goal</div>
-            <div class="rocket-info">Solar system exploration</div>
-
-            <div class="stat-icon">year built</div>
-            <div class="stat-value">1977</div>
-
-            <div class="stat-icon">mission-duration</div>
-            <div class="stat-value">19000</div>
-
-            <div class="stat-icon">max speed</div>
-            <div class="stat-value">62000</div>
-
-            <div class="stat-icon">max earth distance</div>
-            <div class="stat-value">22286000</div>
-
-            <div class="stat-icon">development cost</div>
-            <div class="stat-value">250</div>
-
-            <div class="stat-icon">weight</div>
-            <div class="stat-value">773</div>
+            <div class="rocket-image center-content">
+            <img src="images/${rocket.name}.jpg" alt="${rocket.name}">
         </div>
+            <div class="goal-icon center-content" style="background-color: ${bgColor};">Mission goal</div>
+            <div class="rocket-info">${rocket.mission_goal}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}development.png" alt="development"></div>
+            <div class="stat-value center-content">${rocket.year_built}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}deadline.png" alt="deadline"></div>
+            <div class="stat-value center-content">${rocket.mission_duration}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}speed-test.png" alt="speed-test"></div>
+            <div class="stat-value center-content">${rocket.max_speed}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}dimensions.png" alt="dimensions"></div>
+            <div class="stat-value center-content">${rocket.max_earth_distance}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}money.png" alt="money"></div>
+            <div class="stat-value center-content">${rocket.development_cost}</div>
+
+            <div class="stat-icon center-content"><img src="${iconPath}weight.png" alt="weight"></div>
+            <div class="stat-value center-content">${rocket.weight}</div>
+         </div>
+
+         <div class="card-back"><img src="images/card-back.jpg" alt="card back">
+     </div>
     </div>`);
         wrapper.append(card);
+
     });
-});
 
-
-
-
-        
+    $('.card').on('click', function () {
+        // Toggle the 'card-rotate' class
+        $(this).toggleClass('card-rotate');
+    });
+});       
